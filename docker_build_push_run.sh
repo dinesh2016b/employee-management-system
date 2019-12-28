a85738686584bd25fa3f67f7ef1cea069267b1dd
dinesh2016b@gmail.com <dinesh2016b@gmail.com>
2019-12-28 00:05:52 -0500

    service updated

--- docker_build_push_run.sh
+++ docker_build_push_run.sh
@@ -3,16 +3,16 @@ echo "----------> Maven Clean,Build"
 mvn clean compile install
 
 echo "----------> Docker Build"
-sudo docker build -t dinesh2016b/employee-management-system:0.2 .
+sudo docker build -t dinesh2016b/employee-management-system:0.1 .
 
 echo "----------> Docker images"
 sudo docker images
 
 echo "----------> Docker tag"
-sudo docker tag -t 6fc88282612a dinesh2016b/employee-management-system:0.2
+sudo docker tag -t dinesh2016b/employee-management-system:0.1
 
 echo "----------> Docker push"
-sudo docker push dinesh2016b/employee-management-system:0.2
+sudo docker push dinesh2016b/employee-management-system:0.1
 
 echo "----------> Docker run"
-sudo docker run -t -p 8090:8090 dinesh2016b/employee-management-system:0.2
+sudo docker run -t -p 8090:8090 dinesh2016b/employee-management-system:0.1
