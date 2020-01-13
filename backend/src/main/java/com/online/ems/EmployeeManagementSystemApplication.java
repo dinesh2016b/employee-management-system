e45eb019be82202bd3f4ff4d112da3da0b137a2a
dinesh b <dinesh2016b@gmail.com>
2020-01-13 02:04:51 -0500

    docker image

--- backend/src/main/java/com/online/ems/EmployeeManagementSystemApplication.java
+++ backend/src/main/java/com/online/ems/EmployeeManagementSystemApplication.java
@@ -2,8 +2,12 @@ package com.online.ems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+@RestController
+@EnableAutoConfiguration
 @SpringBootApplication
 public class EmployeeManagementSystemApplication extends SpringBootServletInitializer {
 
