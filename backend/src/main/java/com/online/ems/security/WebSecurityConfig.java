b0e7b6a17aa9907af84672b41698caf0baaf3f5e
Dinesh <dinesh2016b@gmail.com>
2020-03-08 16:17:16 -0400

    updated spring security

--- backend/src/main/java/com/online/ems/security/WebSecurityConfig.java
+++ backend/src/main/java/com/online/ems/security/WebSecurityConfig.java
@@ -3,9 +3,12 @@ package com.online.ems.security;
 import java.util.Arrays;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,34 +16,49 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.cors().and().csrf().disable();
-		
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		.withUser("dinesh")
+		.password("dinesh")
+		.roles("USER")
+		.roles("ADMIN");
 	}
 	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
+	}
+
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+		httpSecurity.cors().and().csrf().disable();
+
+		httpSecurity.authorizeRequests()
+		.antMatchers("/employees").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/departments").hasAnyRole("USER", "ADMIN")
+		.antMatchers("/h2-console/**").permitAll()
+		.antMatchers("/").permitAll()
+		.and().httpBasic();
 		
-		http.cors().and().csrf().disable();
-		
+		httpSecurity.headers().frameOptions().disable();
+
 	}
 
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 
 		CorsConfiguration configuration = new CorsConfiguration();
-		
+
 		configuration.setAllowedOrigins(Arrays.asList("*"));
 		configuration.setAllowedMethods(Arrays.asList("*"));
 		configuration.setAllowedHeaders(Arrays.asList("*"));
 		configuration.setAllowCredentials(true);
-		
+
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
-		
+
 		return source;
 	}
 
