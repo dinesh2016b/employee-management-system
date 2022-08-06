0782d5e5b266a7fc5d530a1df3ff71a2c0cdadf1
Dinesh B <dinesh2016b@gmail.com>
2022-08-05 22:58:17 -0400

    Updated frontend for JWT tokens

--- frontend/src/app/service/salaries.service.ts
+++ frontend/src/app/service/salaries.service.ts
@@ -3,11 +3,13 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs'
 import { Salaries } from '../model/salaries';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 
 export class SalariesService {
 
-  private salariesUrl: string = 'http://localhost:8095/ems-salaries';
+  private salariesUrl: string = 'http://localhost:8080/ems-salaries';
 
   constructor(private http: HttpClient) {
 
